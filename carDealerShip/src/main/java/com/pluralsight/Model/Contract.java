c0953d69d6b60346da964b194c68bc0a909ee670
Fahd <fahds2005@icloud.com>
2024-05-17 10:25:06 -0700

    Contract class completed

--- carDealerShip/src/main/java/com/pluralsight/Model/Contract.java
+++ carDealerShip/src/main/java/com/pluralsight/Model/Contract.java
@@ -1,4 +1,55 @@
 package com.pluralsight.Model;
 
-public class Contract {
+public abstract class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private boolean vehicleSold;
+    private double monthlyPayment;
+    private double totalPrice;
+
+    public Contract(String date, String customerName, String customerEmail, boolean vehicleSold, double monthlyPayment, double totalPrice) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+        this.monthlyPayment = monthlyPayment;
+        this.totalPrice = totalPrice;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public boolean isVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(boolean vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
+    public abstract double getTotal();
+
+    public abstract double getMonthlyPayment();
 }
